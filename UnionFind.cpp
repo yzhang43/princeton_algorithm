fcad6fc644275ed1c365ed548788d67b9591668d
Yantao Zhang <yzhang43@vpn-047.pc.umiacs.umd.edu>
2015-06-19 14:32:00 -0400

    uf add test function

--- UnionFind.cpp
+++ UnionFind.cpp
@@ -1,7 +1,27 @@
 #include <iostream>
+#include <fstream>
 using namespace std;
 
-int main() {
+class UF {
+	UF(int N);
+	void Union(int p, int q);
+	bool connected(int p, int q);
+	int find(int p);
+	int count;
+};
 
+int main() {
+	//test of the program
+	ifstream fin("tinyUF.txt");
+	fin>>N;
+	UF uf = new UF(N);
+	while(!fin.eof()) {
+		int p, q;
+		fin>>p>>q;
+		if(!uf.connected(p, q)) {
+			uf.union(p, q);
+			cout<<p<<" "<<q<<endl;
+		}
+	}
 	return 0;
 }
