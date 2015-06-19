3bf83dc0006c6b3c3d38933efbb4f3d46255f8c9
Yantao Zhang <yzhang43@vpn-047.pc.umiacs.umd.edu>
2015-06-19 14:49:56 -0400

    QuickFind Implementation

--- UnionFind.cpp
+++ UnionFind.cpp
@@ -1,25 +1,47 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
 using namespace std;
 
-class UF {
-	UF(int N);
+class QuickFindUF {
+public:
+	QuickFindUF(int N){
+		for(int i = 0; i < N; ++i)
+			id.push_back(i);
+	}
 	void Union(int p, int q);
 	bool connected(int p, int q);
-	int find(int p);
-	int count;
+	//int find(int p);
+	//int count();
+private:
+	vector<int> id;
 };
+
+void QuickFindUF::Union(int p, int q) {
+	int pid = id[p], qid = id[q];
+	for(int i = 0; i < id.size(); ++i) {
+		if(id[i] == pid)
+			id[i] = qid;
+	}
+}
+
+bool QuickFindUF::connected(int p, int q) {
+	return id[p] == id[q];
+}
+
+
 
 int main() {
 	//test of the program
 	ifstream fin("tinyUF.txt");
+	int N;
 	fin>>N;
-	UF uf = new UF(N);
+	QuickFindUF* uf = new QuickFindUF(N);
 	while(!fin.eof()) {
 		int p, q;
 		fin>>p>>q;
-		if(!uf.connected(p, q)) {
-			uf.union(p, q);
+		if(!uf->connected(p, q)) {
+			uf->Union(p, q);
 			cout<<p<<" "<<q<<endl;
 		}
 	}
