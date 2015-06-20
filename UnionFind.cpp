aa08b95cf302c24376d9d8cde22cb4d668a1f0ae
Yantao Zhang <yzhang43@vpn-047.pc.umiacs.umd.edu>
2015-06-20 13:52:24 -0400

    union find optimize

--- UnionFind.cpp
+++ UnionFind.cpp
@@ -3,18 +3,26 @@
 #include <vector>
 using namespace std;
 
-class QuickFindUF {
+class UF{
 public:
-	QuickFindUF(int N){
+	UF(int N){
 		for(int i = 0; i < N; ++i)
 			id.push_back(i);
 	}
+	virtual void Union(int p, int q) = 0;
+	virtual bool connected(int p, int q) = 0;
+	//int find(int p);
+	//int count();
+	vector<int> id;
+};
+
+class QuickFindUF : public UF {
+public:
+	QuickFindUF(int N) : UF(N){}
 	void Union(int p, int q);
 	bool connected(int p, int q);
 	//int find(int p);
 	//int count();
-private:
-	vector<int> id;
 };
 
 void QuickFindUF::Union(int p, int q) {
@@ -30,13 +38,75 @@ bool QuickFindUF::connected(int p, int q) {
 }
 
 
+class QuickUnionUF : public UF {
+public:
+	QuickUnionUF(int N) : UF(N) {}
+	void Union(int p, int q);
+	bool connected(int p, int q);
+private:
+	int root(int i){
+		while(i != id[i]) {
+			i = id[i];
+		}
+		return i;
+	}
+};
+
+void QuickUnionUF::Union(int p, int q) {
+	int i = root(p);
+	int j = root(q);
+	id[i] = j;
+}
+
+bool QuickUnionUF::connected(int p, int q) {
+	return root(p) == root(q);
+}
+
+
+//weighted quick union with path compression
+class WQUPC : public UF{
+public:
+	WQUPC(int N) : UF(N) {
+		for(int i = 0; i < N; ++i)
+			sz.push_back(1);
+	}
+	void Union(int p, int q);
+	bool connected(int p, int q);
+private:
+	vector<int> sz;
+	int root(int i) {
+		while(i != id[i]) {
+			id[i] = id[id[i]];
+			i = id[i];
+		}
+		return i;
+	}
+};
+
+void WQUPC::Union(int p, int q) {
+	int pid = root(p), qid = root(q);
+	if(pid == qid) return;
+	if(sz[pid] < sz[qid]) {
+		id[pid] = qid; 
+		sz[qid] += sz[pid];
+	}
+	else{
+		id[qid] = id[pid];
+		sz[pid] += sz[qid];
+	}
+}
+
+bool WQUPC::connected(int p, int q) {
+	return root(p) == root(q);
+}
+
 
 int main() {
 	//test of the program
 	ifstream fin("tinyUF.txt");
 	int N;
 	fin>>N;
-	QuickFindUF* uf = new QuickFindUF(N);
+	UF* uf = new WQUPC(N);
 	while(!fin.eof()) {
 		int p, q;
 		fin>>p>>q;
